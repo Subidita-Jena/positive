df0064241a5a021b4d44966b2a6d700e39def199
Subidita-Jena <90219666+Subidita-Jena@users.noreply.github.com>
2021-09-17 18:46:16 +0530

    Add files via upload

---
+++ positive.py
@@ -0,0 +1,8 @@
+list1 = [12,-7,5,64,-14]
+list2 = [12,14,-95,3]
+for elem in list1:
+    if elem>0:
+        print("list1:",elem)
+for element in list2:
+    if element>0:
+        print("list2:",element)        
